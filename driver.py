410b33636add5e6d3035ca519f217ec90e923c42
Andrew Konkol <akonkol@gmail.com>
2012-04-17 16:23:41 -0400

    cleaned up driver.py

--- driver.py
+++ driver.py
@@ -8,8 +8,9 @@ import Systems
 m = Systems.OperatingSystems['IOS']
 s = Connectivity.Session("192.168.1.1",23,"telnet",m)
 s.login("yourusername", "yourpassword")
+
+# if your need to issue an "enable" command
 s.escalateprivileges('yourenablepassword')
-#s.sendcommand("show ver")
 s.sendcommand("show clock")
 s.sendcommand("show run")
 s.logout()
